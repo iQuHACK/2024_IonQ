5f119758d71e1bab7be263be5901939e3d608ebd
declansam <lcsamyam@gmail.com>
2024-02-04 02:09:32 -0500

    create modular cli file

--- cli/ascii.py
+++ cli/ascii.py
@@ -1,11 +1,52 @@
 
-# conda install omegacen/label/broken::asciinet
 
 import networkx as nx
 from asciicode import graph_to_ascii
 
+def createGraph():
+    '''
+    Creates a graph and returns it
+    '''
+    return nx.Graph()
+
+
+def initGraph(graph):
+    '''
+    Initializes the graph with the nodes and returns a dictionary of potential nodes
+    '''
+
+    potential_node_dic = {'q0' : ['q1', 'q2'], 'q1' : ['q3', 'q4']}
+
+    graph.add_node("q0")
+    graph.add_node("q1")
+    graph.add_node("q2")
+
+    return potential_node_dic
+
+
+def logging(graph, log = False):
+    '''
+    Logs the graph in ascii art
+    '''
+
+    ascii_art = graph_to_ascii(graph)
+
+    if log:
+        print(ascii_art)
+
+
+def getPotentialNodeDic():
+    '''
+    Returns the potential node dictionary
+    '''
+
+    return potential_node_dic
+
+
 def connect(q0, qx, graph, potential_node_dic):
-    
+    '''
+    Connects q0 to qx and updates the potential_node_dic
+    '''
     graph.add_edge(q0, qx)
     
     if qx in potential_node_dic.keys():   
@@ -15,28 +56,22 @@ def connect(q0, qx, graph, potential_node_dic):
 
     potential_node_dic[q0].remove(qx)
 
+def setConnection(qx):
+    '''
+    Sets a connection from q0 to qx
+    '''
 
-graph = nx.Graph()
-
-graph.add_node("q0")
-graph.add_node("q1")
-graph.add_node("q2")
-
-potential_node_dic = {'q0' : ['q1', 'q2'], 'q1' : ['q3', 'q4']}
-
-connect('q0', 'q1', graph ,potential_node_dic)
-print("2:\n", potential_node_dic)
-
-connect('q0', 'q2', graph ,potential_node_dic)
-connect('q0', 'q3', graph ,potential_node_dic)
+    connect('q0', qx, graph, potential_node_dic)
+    logging(graph, True)
 
 
+# MAIN
+graph = createGraph()
+potential_node_dic = initGraph(graph)
 
 
-
-# --------------------------------
-ascii_art = graph_to_ascii(graph)
-print(ascii_art)
-# --------------------------------
-
+## TESTING (MODULAR)
+# print(getPotentialNodeDic())
+# setConnection('q1')
+# print(getPotentialNodeDic())
 
